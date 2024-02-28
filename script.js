457f4f034db82b4695b4974f486827b37db1e80b
suresharisetty <147700712+suresharisetty@users.noreply.github.com>
2024-02-28 22:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	  let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 0;
+    for (i = 0; i < num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
+	
 }
+cosole.log(fibonacci(5))
 
-module.exports = fibonacci;
+
